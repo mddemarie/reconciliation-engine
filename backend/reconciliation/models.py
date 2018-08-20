0d98f595e36b292d71a17cef1c433e9a171dbd24
mddemarie <mddemarie@gmail.com>
2018-08-20 12:56:36 +0200

    created all relevant models

--- backend/reconciliation/models.py
+++ backend/reconciliation/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Payable(models.Model):
+    reference_id = models.CharField(max_length=20)
+    amount = models.FloatField()
+    date_occured = models.DateTimeField()
+
+    transaction = models.ForeignKey('Transaction', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.reference_id
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Transaction(models.Model):
+    uid = models.CharField(max_length=20)
+    business = models.ForeignKey('Business', on_delete=models.CASCADE)
+    payment = models.ForeignKey('PaymentLink', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.uid
+
+class Payment(models.Model):
+    info = models.CharField(max_length=20)
+    business = models.ForeignKey('Business', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.info
+
+class PaymentLink(models.Model):
+    payment_date = models.DateField()
